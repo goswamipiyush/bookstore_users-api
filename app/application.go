f6bbb8b8325ecb56ed29415888613c550e10092d
piyushgoswami123 <pgoswami@rocketsoftware.com>
2020-07-13 20:32:16 +0530

    Initial level of changes working for /ping

---
+++ app/application.go
@@ -0,0 +1,20 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func StartApplication() {
+
+	//	mapUrls()
+	router := gin.Default()
+
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	router.Run(":8080")
+}
